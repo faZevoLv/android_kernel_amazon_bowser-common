f5227640f0dc90ba6f5a579ba151313ba22ed200
Jordan Farrington <jordanfaz95@googlemail.com>
2015-01-25 15:47:11 +0000

    Revert adding custom voltages (breaks the build)

--- arch/arm/mach-omap2/dvfs.c
+++ arch/arm/mach-omap2/dvfs.c
@@ -31,10 +31,6 @@
 #ifdef CONFIG_OMAP4_DPLL_CASCADING
 #include <mach/omap4-common.h>
 #endif
-#ifdef CONFIG_CUSTOM_VOLTAGE
-#include <linux/custom_voltage.h>
-#endif
-
 /**
  * DOC: Introduction
  * =================
@@ -257,9 +253,7 @@ static struct omap_vdd_dvfs_info *_dev_to_dvfs_info(struct device *dev)
  *
  * Returns NULL on failure.
  */
-#ifndef CONFIG_CUSTOM_VOLTAGE
 static
-#endif
 struct omap_vdd_dvfs_info *_voltdm_to_dvfs_info(struct voltagedomain *voltdm)
 {
 	struct omap_vdd_dvfs_info *dvfs_info;
@@ -274,9 +268,6 @@ struct omap_vdd_dvfs_info *_voltdm_to_dvfs_info(struct voltagedomain *voltdm)
 
 	return NULL;
 }
-#ifdef CONFIG_CUSTOM_VOLTAGE
-EXPORT_SYMBOL(_voltdm_to_dvfs_info);
-#endif
 
 /* rest of the helper functions */
 /**
@@ -1378,10 +1369,5 @@ int __init omap_dvfs_register_device(struct device *dev, char *voltdm_name,
 	/* Fall through */
 out:
 	mutex_unlock(&omap_dvfs_lock);
-
-#ifdef CONFIG_CUSTOM_VOLTAGE
-	customvoltage_register_dvfsmutex(&omap_dvfs_lock);
-#endif
-
 	return ret;
 }
