80715b0adeef18fd36062599a656ffcbb8d47e79
Jordan Farrington <jordanfaz95@googlemail.com>
2015-01-28 11:11:12 +0000

    Revert "backport SLUB from Linux 3.3 (thanks to faux123)"
    This reverts commit 7130d99e9b7ad447e857d4c78450d2feb4549c00.

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -124,6 +124,9 @@ enum pageflags {
 
 	/* SLOB */
 	PG_slob_free = PG_private,
+
+	/* SLUB */
+	PG_slub_frozen = PG_active,
 };
 
 #ifndef __GENERATING_BOUNDS_H
@@ -209,13 +212,8 @@ PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
 
-
 __PAGEFLAG(SlubFrozen, slub_frozen)
 
-#ifdef CONFIG_KSM_CHECK_PAGE
-CLEARPAGEFLAG(KsmScan0, ksm_scan0) TESTSETFLAG(KsmScan0, ksm_scan0)
-CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
-#endif
 /*
  * Private page markings that may be used by the filesystem that owns the page
  * for its own purposes.
